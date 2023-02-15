e773403bf5bc6a5c0ae7ebf36c08cfcc737de10d
Ezry Edinolfi <62532845+egamez231@users.noreply.github.com>
2023-02-15 23:45:10 +0800

    Add files via upload

---
+++ Quest2Type2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 10;
+
+void ArrIn (int arr[]){
+
+   int i;
+    for (i=0; i < MAX_SIZE; i ++){ // the loop are inserting the data extracted from the line into the array
+        cout << "Input number :" ;
+        cin >> arr[i];
+    }
+}
+
+void MaxMin (int *mx, int *mn,int arr[]){ // function to check the Max and Minimum number in the array
+
+int i = 0;
+
+
+for (i=0; i < MAX_SIZE; i++){ // Loop to find biggest number
+
+    if (arr[0] < arr[i])
+        *mx = arr[i];
+}
+
+*mn = arr[0];
+for (i=0; i < MAX_SIZE; i++){ // Loop to find the smallest number
+
+    if (arr[i]< *mn )
+        *mn = arr[i];
+}
+
+
+}
+
+int OddEven (int arr){ //Boolean to check the number is Odd or Even
+
+if (arr % 2 == 0)
+    return 0;
+else
+    return 1;
+}
+
+
+int main() {
+    int arr[MAX_SIZE],mn,mx, Odd = 0 , Even = 0 ,i; // variable declaration
+
+    cout << "Enter 10 integers:"<< endl;
+    ArrIn (arr);
+    MaxMin (&mx,&mn, arr);
+
+    for (i=0; i < MAX_SIZE; i++){
+        if (OddEven(arr[i])== 1){
+            Even ++;
+        }
+        else{
+            Odd++;
+        }
+
+    }
+
+
+ // display output
+    cout <<"\n"<< "The smallest number: " << mn << endl;
+    cout << "The largest number: " << mx << endl;
+    cout << "Total even elements: " << Even << endl;
+    cout << "Total odd elements: " << Odd << endl;
+
+
+    // use the code below to ease the debugging process for first function.
+  /* print the elements of the array
+    for (int j = 0; j < MAX_SIZE; j++) {
+    cout << arr[j] << " ";
+    }
+    cout << endl;*/
+
+  return 0;
+}
+
+
+
